a308bfd35289e1e6bc2202720837a027d70db5df
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-06-21 18:58:01 +0300

    chore: remove debug from demo

--- playground/react/App.jsx
+++ playground/react/App.jsx
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Mariko from '../../build/react';
 
 const App = () => {
-  const [count, setCount] = useState(0);
   return (
     <div className="container">
-      <p
-        style={{ position: 'absolute', left: 0, top: 0, zIndex: 1000 }}
-        onClick={() => setCount(count + 1)}
-      >
-        {count}
-      </p>
-      <Mariko className="mariko-banner" onEnter={() => console.log('enter', count)}>
+      <Mariko className="mariko-banner" onEnter={() => console.log('enter')}>
         <img className="mariko-banner-spacer" src="./i/mariko-bg.svg" alt="" />
         <img data-mariko-offset="-4.5" src="./i/mariko-bg.svg" alt="" />
         <img data-mariko-offset="-2.5" src="./i/mariko-mountains.svg" alt="" />
