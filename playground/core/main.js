752cb10e83a0e925f2d828e522ae1d0bf45a1b67
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-09-28 11:53:27 +0300

    chore(playground): move JS to HTML

--- playground/core/main.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import Atropos from '../../build/esm/atropos.esm';
-import '../../build/atropos.css';
-
-window.atropos = new Atropos({
-  el: document.querySelectorAll('.atropos')[0],
-  shadowScale: 1.05,
-});
