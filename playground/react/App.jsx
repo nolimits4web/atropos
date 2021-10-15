f3d72ee7046995dacec22cf6d959594785f64e5c
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-10-15 20:20:07 +0300

    chore: roll back react playground

--- playground/react/App.jsx
+++ playground/react/App.jsx
@@ -3,34 +3,16 @@ import Atropos from '../../build/react';
 
 const App = () => {
   return (
-    <div className={`atropos-multiple`}>
-      {Array.from({ length: 9 }).map((_, index) => {
-        const row = Math.floor(index / 3);
-        const col = index - row * 3;
-        return (
-          <Atropos
-            // eslint-disable-next-line
-            key={index}
-            eventsEl=".atropos-multiple"
-            rotateTouch="scroll-y"
-            rotateXMax={10}
-            rotateYMax={10}
-            shadowOffset={100}
-            rotateXInvert={true}
-            rotateYInvert={true}
-            stretchX={20}
-            stretchY={30}
-            duration={400}
-            data-index={index}
-          >
-            <div
-              data-atropos-offset="8"
-              className="atropos-multiple-bg"
-              style={{ backgroundPosition: `${-col * 105}% ${-row * 105}%` }}
-            />
-          </Atropos>
-        );
-      })}
+    <div className="container">
+      <Atropos className="atropos-banner" highlight={false} onEnter={() => console.log('enter')}>
+        <img className="atropos-banner-spacer" src="./i/atropos-bg.svg" alt="" />
+        <img data-atropos-offset="-4.5" src="./i/atropos-bg.svg" alt="" />
+        <img data-atropos-offset="-2.5" src="./i/atropos-mountains.svg" alt="" />
+        <img data-atropos-offset="0" src="./i/atropos-forest-back.svg" alt="" />
+        <img data-atropos-offset="2" src="./i/atropos-forest-mid.svg" alt="" />
+        <img data-atropos-offset="4" src="./i/atropos-forest-front.svg" alt="" />
+        <img data-atropos-offset="5" src="./i/atropos-logo-en.svg" alt="" />
+      </Atropos>
     </div>
   );
 };
