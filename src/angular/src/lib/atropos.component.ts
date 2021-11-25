e237bdc5cccc6f05863c82676e4a5eade3e18f3e
Vlad Tansky <vl.tansky@gmail.com>
2021-11-25 11:07:09 +0200

    feat(angular): init

--- src/angular/src/lib/atropos.component.ts
+++ src/angular/src/lib/atropos.component.ts
@@ -1,5 +1,13 @@
-import { Component, ElementRef, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import AtroposCore, { AtroposInstance } from 'atropos';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  SimpleChanges,
+} from '@angular/core';
+import AtroposCore, { AtroposInstance, AtroposOptions } from 'atropos';
 
 @Component({
   selector: 'atropos',
@@ -17,31 +25,31 @@ import AtroposCore, { AtroposInstance } from 'atropos';
   styles: [],
 })
 export class AtroposComponent implements OnInit {
-  @Input() shadow: boolean | undefined;
-  @Input() highlight: boolean | undefined;
   @Input() class: string = '';
-  @Input() eventsEl: any;
-  @Input() alwaysActive: any;
-  @Input() activeOffset: any;
-  @Input() shadowOffset: any;
-  @Input() shadowScale: any;
-  @Input() duration: any;
-  @Input() rotate: any;
-  @Input() rotateTouch: any;
-  @Input() rotateXMax: any;
-  @Input() rotateYMax: any;
-  @Input() rotateXInvert: any;
-  @Input() rotateYInvert: any;
-  @Input() stretchX: any;
-  @Input() stretchY: any;
-  @Input() stretchZ: any;
-  @Input() commonOrigin: any;
+  @Input() eventsEl: AtroposOptions['eventsEl'];
+  @Input() shadow: AtroposOptions['shadow'];
+  @Input() highlight: AtroposOptions['highlight'];
+  @Input() alwaysActive: AtroposOptions['alwaysActive'];
+  @Input() activeOffset: AtroposOptions['activeOffset'];
+  @Input() shadowOffset: AtroposOptions['shadowOffset'];
+  @Input() shadowScale: AtroposOptions['shadowScale'];
+  @Input() duration: AtroposOptions['duration'];
+  @Input() rotate: AtroposOptions['rotate'];
+  @Input() rotateTouch: AtroposOptions['rotateTouch'];
+  @Input() rotateXMax: AtroposOptions['rotateXMax'];
+  @Input() rotateYMax: AtroposOptions['rotateYMax'];
+  @Input() rotateXInvert: AtroposOptions['rotateXInvert'];
+  @Input() rotateYInvert: AtroposOptions['rotateYInvert'];
+  @Input() stretchX: AtroposOptions['stretchX'];
+  @Input() stretchY: AtroposOptions['stretchY'];
+  @Input() stretchZ: AtroposOptions['stretchZ'];
+  @Input() commonOrigin: AtroposOptions['commonOrigin'];
   @Input() scaleClassName: string = '';
   @Input() rotateClassName: string = '';
   @Input() innerClassName: string = '';
-  @Output() onEnter = new EventEmitter();
-  @Output() onLeave = new EventEmitter();
-  @Output() onRotate = new EventEmitter();
+  @Output() onEnter = new EventEmitter<Parameters<AtroposOptions['onEnter']>>();
+  @Output() onLeave = new EventEmitter<Parameters<AtroposOptions['onLeave']>>();
+  @Output() onRotate = new EventEmitter<Parameters<AtroposOptions['onRotate']>>();
   ref: AtroposInstance | undefined;
   constructor(private elementRef: ElementRef) {}
 
@@ -51,7 +59,34 @@ export class AtroposComponent implements OnInit {
     this.initAtropos();
   }
 
+  getConfig() {
+    return {
+      eventsEl: this.eventsEl,
+      alwaysActive: this.alwaysActive,
+      activeOffset: this.activeOffset,
+      shadowOffset: this.shadowOffset,
+      shadowScale: this.shadowScale,
+      duration: this.duration,
+      rotate: this.rotate,
+      rotateTouch: this.rotateTouch,
+      rotateXMax: this.rotateXMax,
+      rotateYMax: this.rotateYMax,
+      rotateXInvert: this.rotateXInvert,
+      rotateYInvert: this.rotateYInvert,
+      stretchX: this.stretchX,
+      stretchY: this.stretchY,
+      stretchZ: this.stretchZ,
+      commonOrigin: this.commonOrigin,
+      shadow: this.shadow,
+      highlight: this.highlight,
+    };
+  }
+
+  ngOnChanges(changedParams: SimpleChanges) {
+    this.ref.params = this.getConfig();
+  }
+
   initAtropos() {
-    this.ref = AtroposCore({ el: this.elementRef.nativeElement });
+    this.ref = AtroposCore({ el: this.elementRef.nativeElement, ...this.getConfig() });
   }
 }
